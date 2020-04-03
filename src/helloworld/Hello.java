960c21b1af2e91e1ee21c13527c24160f2478ada
Siva-Saru <Siva-Saru@Tunnu-Tuttu>
2020-04-03 10:35:32 +0200

    First commit...

---
+++ src/helloworld/Hello.java
@@ -0,0 +1,15 @@
+package helloworld;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float temp =(float) 36.0 ;
+		
+				if(temp <= 20)
+					System.out.println("Its normal temperatutre");
+				else
+						System.out.print("its very hot");
+	}
+
+}
